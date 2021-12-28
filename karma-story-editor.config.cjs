1f0bf8fcf02fbe5c40104401b8cf615c3f59a943
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2021-12-28 16:08:54 +0530

    WIP

--- karma-story-editor.config.cjs
+++ karma-story-editor.config.cjs
@@ -62,6 +62,15 @@ module.exports = function (config) {
     files: [
       { pattern: 'packages/story-editor/src/karma-tests.cjs', watched: false },
       { pattern: 'packages/karma-fixture/src/init.js', watched: false },
+      'https://fonts.googleapis.com/css2?display=auto&family=Roboto%3Aital%2Cwght%400%2C100%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C700%3B0%2C900%3B1%2C100%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C700%3B1%2C900',
+      {
+        pattern:
+          'https://fonts.googleapis.com/css2?display=auto&family=Roboto%3Aital%2Cwght%400%2C100%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C700%3B0%2C900%3B1%2C100%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C700%3B1%2C900',
+        type: 'css',
+        included: true,
+        served: true,
+        nocache: false,
+      },
       {
         pattern: '__static__/**/*',
         watched: false,
