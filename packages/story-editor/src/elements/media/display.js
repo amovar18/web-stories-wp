c173119b8036b8cb7eef17441365ae05283c641d
Kudaligi Amoghavarsha <kgamoghavarsha@gmail.com>
2022-01-25 10:55:25 +0530

    useTransformHandler references to new package

--- packages/story-editor/src/elements/media/display.js
+++ packages/story-editor/src/elements/media/display.js
@@ -20,7 +20,7 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { getMediaSizePositionProps } from '@googleforcreators/media';
-
+import { useTransformHandler } from '@googleforcreators/transform';
 /**
  * Internal dependencies
  */
@@ -32,7 +32,6 @@ import {
   elementWithBorder,
   elementWithBackgroundColor,
 } from '../shared';
-import { useTransformHandler } from '../../components/transform';
 import {
   getResponsiveBorder,
   shouldDisplayBorder,
--- packages/story-editor/src/elements/shared/useCSSVarColorTransformHandler.js
+++ packages/story-editor/src/elements/shared/useCSSVarColorTransformHandler.js
@@ -15,9 +15,9 @@
  */
 
 /**
- * Internal dependencies
+ * External dependencies
  */
-import { useTransformHandler } from '../../components/transform';
+import { useTransformHandler } from '@googleforcreators/transform';
 
 function useCSSVarColorTransformHandler({
   id,
